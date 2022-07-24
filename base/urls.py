aa5da2d253dce3b2ed5cfb5c84fb5a7fa665f798
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-24 23:21:22 +0530

    Add: setup basic view and templates

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+# Views
+from . import views
+
 urlpatterns = [
-    
+    path('', views.home, name="home"),
+    path('room/<str:pk>', views.room, name="room"),
+    path('/create-room', views.createRoom, name="create-room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Models
+from .models import Room
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+     
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
