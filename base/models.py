5595a3450206ccf7c204846c17ef7aa4a23378e0
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-24 23:01:08 +0530

    Add: room, user, message model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) :
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=450, null=True, blank=True)
+    # participants = 
+    updated = models.DateTimeField(auto_now=True) #update everytime
+    created = models.DateTimeField(auto_now_add=True) #update intially
+
+    def __str__(self) :
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True) #update everytime
+    created = models.DateTimeField(auto_now_add=True) #update intially
+
+    def __str__(self) :
+        return self.body[0:50]
