8f78fc71b5b2602fc91c96f12a574b23bfdde258
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-25 03:04:30 +0530

    Add: Login authentication

--- base/views.py
+++ base/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
 
 # Models
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
+
+# Login
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if(user is not None):
+            login(request, user)
+            messages.success(request, "Successfully logged in.")
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password incorrect.")
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
 
 # Home Page
 def home(request):
