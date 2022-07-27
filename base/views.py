8d9a3d3e57a56ab72a464416808dbe5ddfcb136e
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-27 22:35:24 +0530

    Update: filter activity by room

--- base/views.py
+++ base/views.py
@@ -71,7 +71,7 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
