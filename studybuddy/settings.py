ff499cf4ca6b34c1bc086a24f33ed23b93f700e7
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-29 01:52:18 +0530

    Undo staticroot path

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
